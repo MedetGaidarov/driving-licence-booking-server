b60c1c713d916d72b43f443afc70b60248f061dc
= <mgaiidarov@gmail.com>
2023-05-05 03:16:08 +0500

    init commit

---
+++ src/main/java/com/example/drivinglicenceserver/DrivingLicenceServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.drivinglicenceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingLicenceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivingLicenceServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/drivinglicenceserver/repository/TestCenterRepository.java
@@ -0,0 +1,9 @@
+package com.example.drivinglicenceserver.repository;
+
+import com.example.booking.model.TestCenter;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TestCenterRepository extends JpaRepository<TestCenter, Long> {
+}
